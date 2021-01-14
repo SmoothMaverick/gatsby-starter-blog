66696b5f76034eea85aea313c5c9904747b7234f
jason <5660582+SmoothMaverick@users.noreply.github.com>
2021-01-13 18:29:17 -0800

    Format

--- src/components/layout.test.js
+++ src/components/layout.test.js
@@ -1,17 +1,12 @@
-import React from 'react'
-import renderer from 'react-test-renderer'
+import React from "react"
+import renderer from "react-test-renderer"
 import Layout from "./layout.js"
 
 describe("Layout", () => {
   it("renders correctly", () => {
-    const layout = <Layout
-      title="Title"
-      location="/"
-    />
+    const layout = <Layout title="Title" location="/" />
 
-    const tree = renderer
-      .create(layout)
-      .toJSON()
+    const tree = renderer.create(layout).toJSON()
     expect(tree).toMatchSnapshot()
   })
 })
